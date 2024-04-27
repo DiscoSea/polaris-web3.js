08245ee6a45c1d2779a69d04d5e59a672141b590
azuldev <91290695+redazul@users.noreply.github.com>
2024-04-26 22:04:31 -0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,16 @@
 import { PublicKey, TransactionInstruction, SystemProgram } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID} from '@solana/spl-token';
 
-const CONFIG_PDA = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
-const POLARIS_SOL = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
-const POLARIS_ATLAS = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
+const POLARIS_CONFIG_PDA = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
+const POLARIS_FEE_SOL = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
+const POLARIS_FEE_ATLAS = new PublicKey("FgWVFPpFQpAk1NkXn6X7q9rUtk8sfq3nEbxE9THt2nmD");
+const programId = new PublicKey('9NVKKHGxkMQPDpemz6ZTdRrM9P4dxaKGRcjgTBncVDRm');
 
-export const programId = new PublicKey('9NVKKHGxkMQPDpemz6ZTdRrM9P4dxaKGRcjgTBncVDRm');
+const Instructions =
+{
+  BuyInstruction:1
+}
+
 
 module.exports = {
     greet: function() {
@@ -21,17 +26,20 @@ module.exports = {
       resourceMint:PublicKey,
       userResourceAccount:PublicKey,
       pdaAmmoAccount:PublicKey,
-      beneficiaryAtlasAccount:PublicKey
+      beneficiaryAtlasAccount:PublicKey,
+      multiplier:number
     )
     {
-        //user is buying resource 
-      let ix2 = 1; // Example command or identifier
-      let ixBuffer2 = Buffer.alloc(1); // Allocate a buffer for 1 byte
-      ixBuffer2.writeUInt8(ix2, 0); // Write the value of ix at index 0
+
+      let ixBuffer2 = Buffer.alloc(1); 
+      ixBuffer2.writeUInt8(Instructions.BuyInstruction, 0);
+
+      let ammountBuffer = Buffer.alloc(1); 
+      ammountBuffer.writeUInt8(multiplier, 0); 
       
       // Combine all parts into one instruction data buffer
       const instructionData2 = Buffer.concat([
-          ixBuffer2
+          ixBuffer2,ammountBuffer
       ]);
       
       const instruction2 = new TransactionInstruction({
@@ -44,12 +52,12 @@ module.exports = {
               { pubkey: resourceMint, isSigner: false, isWritable: true },
               { pubkey: userResourceAccount, isSigner: false, isWritable: true },
               { pubkey: pdaAmmoAccount, isSigner: false, isWritable: true },
-              { pubkey: POLARIS_ATLAS, isSigner: false, isWritable: true }, //fee star atlas account
+              { pubkey: POLARIS_FEE_ATLAS, isSigner: false, isWritable: true }, //fee star atlas account
               { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },
               { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
               { pubkey: beneficiaryAtlasAccount, isSigner: false, isWritable: true }, //beneficiary atlas account
-              { pubkey: CONFIG_PDA, isSigner: false, isWritable: false },
-              { pubkey: POLARIS_SOL, isSigner: false, isWritable: true } //fee sol account
+              { pubkey: POLARIS_CONFIG_PDA, isSigner: false, isWritable: false },
+              { pubkey: POLARIS_FEE_SOL, isSigner: false, isWritable: true } //fee sol account
           ],
           programId: programId,
           data: instructionData2
