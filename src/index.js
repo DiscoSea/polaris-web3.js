5ccb2b1eaa4b23e2670d452a5964cced93cdff8f
redazul <azuldevgames@gmail.com>
2024-04-29 22:48:49 -0400

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 const POLARIS_CONFIG_PDA = new PublicKey("9Qn1FLErYHPZiRusHdGvQDgVJeR82VrEXbYf33fHAvmB");
 
 const Instructions = {
+    VaultInstruction:0,
     BuyInstruction: 1,
     SellInstruction:2
 };
@@ -12,6 +13,70 @@ const Instructions = {
 
 export function greet() {
   console.log("Hello from @polaris-fuel/web3.js");
+}
+
+export function createVaultInstruction(
+  feePayerPk,
+  resourceMint,
+  marketPDA,
+  minimum_buy_qty,
+  buy_price,
+  minimum_sell_qty,
+  sell_price,
+  beneficiary_atlast_account_buffer,
+  beneficiary_resource_account_buffer,
+  beneficiary_percent
+)
+{
+  let ix = instruction.VaultInstruction; // Example command or identifier
+  let ixBuffer = Buffer.alloc(1); // Allocate a buffer for 1 byte
+  ixBuffer.writeUInt8(ix, 0); // Write the value of ix at index 0
+
+  // Quantities as unsigned 64-bit integers
+  let minBuyQtyBuffer = Buffer.alloc(8);
+  minBuyQtyBuffer.writeBigUInt64LE(BigInt(minimum_buy_qty), 0);
+
+  // Prices as 64-bit floating-point numbers
+  let buyPriceBuffer = Buffer.alloc(8);
+  buyPriceBuffer.writeDoubleLE(buy_price, 0);
+
+  let minSellQtyBuffer = Buffer.alloc(8);
+  minSellQtyBuffer.writeBigUInt64LE(BigInt(minimum_sell_qty), 0);
+
+  let sellPriceBuffer = Buffer.alloc(8);
+  sellPriceBuffer.writeDoubleLE(sell_price, 0);
+
+  let beneficiaryPercentBuffer = Buffer.alloc(4); // Allocate a buffer for 4 bytes (32-bit float)
+  beneficiaryPercentBuffer.writeFloatLE(beneficiary_percent, 0);
+
+
+  // Combine all parts into one instruction data buffer
+  const instructionData = Buffer.concat([
+      ixBuffer,
+      minBuyQtyBuffer,
+      buyPriceBuffer,
+      minSellQtyBuffer,
+      sellPriceBuffer,
+      beneficiary_atlast_account_buffer, // Assuming this is already a Buffer of 32 bytes
+      beneficiary_resource_account_buffer, // Assuming this is already a Buffer of 32 bytes
+      beneficiaryPercentBuffer
+  ]);
+
+  console.log("Length of the buffer: " + instructionData.length);
+
+  // Create the instruction
+  const instruction = new TransactionInstruction({
+      keys: [
+          { pubkey: feePayerPk, isSigner: true, isWritable: true },
+          { pubkey: resourceMint, isSigner: false, isWritable: false },
+          { pubkey: marketPDA, isSigner: false, isWritable: true },
+          { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
+      ],
+      programId: programId,
+      data: instructionData
+  });
+
+  return instruction
 }
 
 
